23ea620249bd15b09eec125ccf0e4c97bc67c7ed
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2020-04-14 15:09:09 -0300

    Add files via upload

---
+++ Tabuada.py
@@ -0,0 +1,10 @@
+n1 = 1
+while n1 <= 10:
+    a = 1
+    n2 = int(input('digite um valor:  '))
+    while a <= 10:
+        print ('%d x %d = %d' % (n2, n1, n1*n2))
+        a = a +1
+        n1 = n1 + 1
+
+   
