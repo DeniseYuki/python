555d5a3c55d1867b73d875fcd24d4218d551ef76
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2020-12-11 11:53:13 -0300

    Add files via upload

--- arvore de natal.py
+++ arvore de natal.py
@@ -52,25 +52,3 @@ t.forward(140)
 
 
 
-#for x in range(180):
-  #t.forward(1)
-   # t.right(1)
-#t.right(180) # forma-se a metade do coração lado esquerdo
-#t.forward(1)
-#t = turtle.Pen()
-#t.left(15)
-#t.right(1)
-#metade do coração lado direito
-#t = turtle.Pen()
-#t.left(-90)
-#t.color('red')
-#t.right(-305)
-#t.forward(-80)
-#for x in range(180):
-    #t.forward(-1)
-    #t.right(-1)
-#t.right(180)
-#t.forward(1)
-#t.left(-15)
-#t.right(-1)
-#fim novo cod do lado do coração direito
