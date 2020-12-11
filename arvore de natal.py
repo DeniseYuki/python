661ec2cc5c8ee7e3adef5243926f95ce6e84b72d
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2020-12-11 11:51:54 -0300

    Add files via upload

---
+++ arvore de natal.py
@@ -0,0 +1,76 @@
+import turtle
+t = turtle.Pen()
+#parte direita
+#primeira parte da árvore
+t.left(80) 
+t.color('green')
+t.right(300)             
+t.forward(-120)
+t.left(40)
+t.forward(50)
+#fim da primeira parte da árvore e ínicio da segunda
+t.left(80)
+t.right(300)
+t.forward(115)
+t.left(40)
+t.forward(-90)
+#t.forward(80)
+#t.left(45)
+# f. da segunda inicio da 3ª
+t.left(80)
+t.right(300)
+t.forward(-140)
+t.left(40)
+t.forward(115)
+# tronco
+t.color('brown')
+t.right(-90)
+t.forward(100)
+t.left(-90)
+t.forward(75)
+t.right(90)
+t.forward(100)
+# fim da parte direita e inicio da parte esquerda da árvore
+#parte da folha de baixo
+t.color('green')
+t.right(-90)
+t.forward(115)
+t.left(-140)
+t.forward(140)
+t.right(-140)
+t.forward(95)
+t.right(145)
+t.forward(125)
+t.right(-145)
+t.forward(65)
+# fim da segunda
+t.right(-215)
+t.forward(140)
+
+
+
+
+
+
+#for x in range(180):
+  #t.forward(1)
+   # t.right(1)
+#t.right(180) # forma-se a metade do coração lado esquerdo
+#t.forward(1)
+#t = turtle.Pen()
+#t.left(15)
+#t.right(1)
+#metade do coração lado direito
+#t = turtle.Pen()
+#t.left(-90)
+#t.color('red')
+#t.right(-305)
+#t.forward(-80)
+#for x in range(180):
+    #t.forward(-1)
+    #t.right(-1)
+#t.right(180)
+#t.forward(1)
+#t.left(-15)
+#t.right(-1)
+#fim novo cod do lado do coração direito
